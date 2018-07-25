3895b1731b9bfebf639bec11b76fd92cdc6aff89
Ron Clark <ronaldtclark@gmail.com>
2018-07-25 16:12:55 -0500

    cat edit route

--- controllers/authController.js
+++ controllers/authController.js
@@ -55,20 +55,20 @@ router.post('/register', (req, res) => {
   userDbEntry.username = req.body.username
   userDbEntry.password = passwordHash
   User.create(userDbEntry, (err, user) => {
-    userDbEntry.username = req.session.username 
-    userDbEntry.firstName = req.session.firstName
-    userDbEntry.lastName = req.session.lastName
-    userDbEntry.email = req.session.email
-    userDbEntry.phone = req.session.phone
-    userDbEntry.age = req.session.age 
-    userDbEntry.maritalStatus = req.session.maritalStatus
-    userDbEntry.children = req.session.children
-    userDbEntry.childrenAges = req.session.childrenAges
-    userDbEntry.yard = req.session.yard
-    userDbEntry.otherPets = req.session.otherPets
+    userDbEntry.username = req.session.username, 
+    userDbEntry.firstName = req.session.firstName,
+    userDbEntry.lastName = req.session.lastName,
+    userDbEntry.email = req.session.email,
+    userDbEntry.phone = req.session.phone,
+    userDbEntry.age = req.session.age, 
+    userDbEntry.maritalStatus = req.session.maritalStatus,
+    userDbEntry.children = req.session.children,
+    userDbEntry.childrenAges = req.session.childrenAges,
+    userDbEntry.yard = req.session.yard,
+    userDbEntry.otherPets = req.session.otherPets,
     req.session.loggedIn = true
-    res.redirect('/users/new')
   })
+    res.redirect('/users/new')
 })
 
 // LOG IN
--- controllers/userController.js
+++ controllers/userController.js
@@ -57,41 +57,34 @@ router.get('/:id/edit', async (req, res) => {
 
 
 //UPDATE
-router.put('/:id', async (req, res) => {
-  //.get the user  based on username in session
-  // update that user
-  // user.save()
-  console.log(req.body)
-
-  try {
+router.put('/:id', (req, res) => {
     // const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {new: true})
-    const foundUser = await User.findOne({userId: req.session.id})
-    User.update(userDbEntry, (err, user) => {
-      req.session.firstName = user.firstName
-      req.session.lastName = user.lastName
-      req.session.email = user.email
-      req.session.phone = user.phone
-      req.session.age = user.age
-      req.session.maritalStatus = user.maritalStatus
-      req.session.children = user.children
-      req.session.childrenAges =  user.childrenAges
-      req.session.yard = user.yard
-      req.session.otherPets = user.otherPets
-    })
+    const foundUser = User.findById({userId: req.session.id})
+    const doc = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      phone: req.body.phone,
+      age: req.body.age,
+      maritalStatus: req.body.maritalStatus,
+      children: req.body.children,
+      childrenAges: req.body.childrenAges,
+      yard: req.body.yard,
+      otherPets: req.body.otherPets
+    }
+    User.update(foundUser, doc, (err, user) => {
     console.log(foundUser)
 
     foundUser.save()
     res.redirect('/users/userId')
-  } catch (err) {
-    res.send(err)
-  }
-})
+  }) 
+  })
 
 
 //POST
 router.post('/', async (req, res) => {
   try {
-    const createdUser = await User.create(req.body)
+    const createdUser = await User.update(req.body)
     res.redirect('/')
   } catch (err) {
     res.send(err)
