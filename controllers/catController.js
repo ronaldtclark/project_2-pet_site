96bcdd46df3e2a9c97aa0566fdb3903f8e15d0e6
Justin Evangelista <justin.j.evangelista@gmail.com>
2018-07-27 02:40:23 -0500

    mostly done

--- controllers/catController.js
+++ controllers/catController.js
@@ -62,7 +62,53 @@ router.get('/:id', async (req, res) => {
   }
 })
 
-// 
+// EDIT
+router.get('/:id/edit', async (req, res) => {
+  try {
+    const foundCat = await Cat.findById(req.params.id)
+    res.render('cats/edit.ejs', {
+      cat: foundCat,
+      userId: req.session.id
+    })
+  } catch (err) {
+    res.send (err)
+  }
+})
+
+// UPDATE
+router.put('/:id', async (req, res) => {
+  if(req.body.goodWithKids === 'on') {
+    req.body.goodWithKids = true
+  } else {
+    req.body.goodWithKids = false
+  }
+  if(req.body.goodWithPets === 'on') {
+    req.body.goodWithPets = true
+  } else {
+    req.body.goodWithPets = false
+  }
+  if(req.body.highEnergy === 'on') {
+    req.body.highEnergy = true
+  } else {
+    req.body.highEnergy = false
+  }
+  try {
+    const updatedCat = await Cat.findByIdAndUpdate(req.params.id, req.body, {new:true})
+    res.redirect('/cats')
+  } catch (err) {
+    res.send (err)
+  }
+})
+
+// DELETE
+router.delete('/:id', async (req, res) => {
+  try {
+    const deletedCat = await Cat.findByIdAndRemove(req.params.id)
+    res.redirect('/cats')
+  } catch (err) {
+    res.send (err)
+  }
+})
 
 
 
